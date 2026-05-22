1c03e729ead866915a7f91c6c863d5e289b443b7
Samara Alanna <sami_andrade@outlook.com.br>
2026-05-21 22:33:08 -0300

    Adiciona mensagem pré-preenchida no link WhatsApp do Suporte Mensal

--- servicos.php
+++ servicos.php
@@ -61,7 +61,7 @@
               <div class="servico-price">R$400</div>
               <div class="servico-prazo">Contrato mensal renovável</div>
             </div>
-            <a href="https://wa.me/5541920058670" class="btn-servico" target="_blank">Solicitar serviço</a>
+            <a href="https://wa.me/5541920058670?text=Ol%C3%A1%2C%20vim%20pelo%20site%20e%20tenho%20interesse%20no%20Suporte%20Mensal!" class="btn-servico" target="_blank">Solicitar serviço</a>
           </div>
         </div>
 
